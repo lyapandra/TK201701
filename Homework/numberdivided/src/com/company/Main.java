36f5b3e902e97ccb97e46e2f78d0e5d5c077fca8
yura.kruhlyk <murove1q@gmail.com>
2017-03-04 14:47:45 +0200

    first homework

---
+++ Homework/numberdivided/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введіть кількість чисел(наприклад 1000):");
+        int input = in.nextInt();
+        for (int i = 1; i <=input; i++) {
+            if ((i % 3) == 0 && (i % 9) == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
